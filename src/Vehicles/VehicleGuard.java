5b6d1fca4a7f234871b785bea37c556eadfbaebb
CamiloGJ <81783581+CamiloGJ@users.noreply.github.com>
2021-05-08 13:12:02 -0500

    VehicleGuardState

---
+++ src/Vehicles/VehicleGuard.java
@@ -0,0 +1,11 @@
+package User;
+
+import BESA.Kernel.Agent.Event.EventBESA;
+import BESA.Kernel.Agent.GuardBESA;
+
+public class VehicleGuard extends GuardBESA {
+    @Override
+    public void funcExecGuard(EventBESA eventBESA) {
+
+    }
+}
