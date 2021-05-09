8a1939b98077815ba58a403e2636feb4f6d073ad
CamiloGJ <81783581+CamiloGJ@users.noreply.github.com>
2021-05-09 12:23:16 -0500

    Vehciles changes

---
+++ src/Vehicles/VehicleData.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Vehicles;
+
+/**
+ *
+ * @author camil
+ */
+import java.util.*;
+import java.util.function.Supplier;
+import BESA.Kernel.Agent.Event.DataBESA;
+
+public class VehicleData extends DataBESA {
+    public final int buses = 3;
+    
+    public final int carros = buses * 2;
+
+    public final int bicicletas = carros *2;
+    
+    public final String[] vehTypes = {"bus", "carro", "bicicleta"};
+
+    //public final String[] placesNodes = {"kennedy", "soacha", "bosa", "centro", "chapienro", "suba", "engativa", "teusaquillo"};
+    
+    
+
+    for (int i = 0; i < buses+carros+bicicletas; i++) {
+    Agents.add}
+  /*  private List<String> vehTypes = new ArrawList<String>();
+    vehTypes.add("bus");
+    vehTypes.add("carro");
+    vehTypes.add("bicicleta");
+//vehTypes = ["bus", "carro", "bicicleta"];
+    private List<String> placesNodes = new ArrawList<String>();
+    placesNodes.add("kennedy");
+    placesNodes.add("soacha");
+    placesNodes.add("bosa");
+    placesNodes.add("centro");
+    placesNodes.add("chapienro");
+    placesNodes.add("suba");
+    placesNodes.add("engativa");
+    placesNodes.add("teusaquillo");
+ //placesNodes = ["kennedy", "soacha", "bosa", "centro", "chapienro", "suba", "engativa", "teusaquillo"];  
+    */
+    
+
+    
+    public class 
+}
